01f2817573f758e0c497ef3b7914484659195e19
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-28 20:46:12 +0200

    Initial commit

---
+++ C++_Design_Pattern_Project/C++_Design_Pattern_Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++_Design_Pattern_Project
+//
+//  Created by Henrik Jangefelt on 2020-09-28.
+//  Copyright © 2020 Henrik Jangefelt Nilsson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
