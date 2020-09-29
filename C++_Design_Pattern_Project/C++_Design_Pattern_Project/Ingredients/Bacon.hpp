1f511734906c5049ea2a8162e14f8de350a0bd5b
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-29 22:29:16 +0200

    A Bacon class that inherits from Ingredient
    class automatically sets nutrition information

---
+++ C++_Design_Pattern_Project/C++_Design_Pattern_Project/Ingredients/Bacon.hpp
@@ -0,0 +1,37 @@
+//
+//  Bacon.hpp
+//  C++_Design_Pattern_Project
+//
+//  Created by Henrik Jangefelt on 2020-09-29.
+//  Copyright © 2020 Henrik Jangefelt Nilsson. All rights reserved.
+//
+
+#ifndef Bacon_hpp
+#define Bacon_hpp
+
+#include "Ingredient.hpp"
+#include <stdio.h>
+#include <string>
+
+namespace burger_program {
+
+class Bacon : public Ingredient
+{
+private:
+    std::string m_baconType;
+    
+public:
+    Bacon(const std::string &type, int weight, bool isVegetarian)
+        : Ingredient{ weight, isVegetarian },m_baconType{ type }
+    {
+        setIngredientNutrition();
+    }
+    
+    void setIngredientNutrition();
+    
+    const std::string& getBaconType() const { return m_baconType; }
+};
+
+}
+
+#endif /* Bacon_hpp */
