aadc759aa3bdc3129c988c1099ffa42826745059
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-29 22:29:48 +0200

    Cheese class that inherits from Ingredient

---
+++ C++_Design_Pattern_Project/C++_Design_Pattern_Project/Ingredients/Cheese.hpp
@@ -0,0 +1,38 @@
+//
+//  Cheese.hpp
+//  C++_Design_Pattern_Project
+//
+//  Created by Henrik Jangefelt on 2020-09-29.
+//  Copyright © 2020 Henrik Jangefelt Nilsson. All rights reserved.
+//
+
+#ifndef Cheese_hpp
+#define Cheese_hpp
+
+#include "Ingredient.hpp"
+#include <stdio.h>
+#include <string>
+
+namespace burger_program {
+
+class Cheese : public Ingredient
+{
+private:
+    std::string m_cheeseType;
+    
+public:
+    Cheese(const std::string &type, int weight, bool isVegetarian)
+        : Ingredient{ weight, isVegetarian }, m_cheeseType{ type }
+    {
+        setIngredientNutrition();
+    }
+    
+    void setIngredientNutrition();
+    
+    const std::string& getCheeseType() const { return m_cheeseType; }
+    
+};
+
+}
+
+#endif /* Cheese_hpp */
