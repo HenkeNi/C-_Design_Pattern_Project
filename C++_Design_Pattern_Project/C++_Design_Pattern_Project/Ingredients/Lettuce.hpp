4f41a071e6c4602a711504ede9001668e5025fe7
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-29 22:30:17 +0200

    Lettuce class that inherits from ingredient
    also contains composition of NutritionFact class

---
+++ C++_Design_Pattern_Project/C++_Design_Pattern_Project/Ingredients/Lettuce.hpp
@@ -0,0 +1,36 @@
+//
+//  Lettuce.hpp
+//  C++_Design_Pattern_Project
+//
+//  Created by Henrik Jangefelt on 2020-09-28.
+//  Copyright © 2020 Henrik Jangefelt Nilsson. All rights reserved.
+//
+
+#ifndef Lettuce_hpp
+#define Lettuce_hpp
+
+#include "Ingredient.hpp"
+#include <stdio.h>
+#include <string>
+
+namespace burger_program {
+
+class Lettuce : public Ingredient
+{
+private:
+    std::string m_lettuceSort;
+    
+public:
+    Lettuce(const std::string &sort, int weight, bool isVegetarian = true)
+        : Ingredient{ weight, isVegetarian }, m_lettuceSort{ sort }
+    {
+        setIngredientNutrition();
+    }
+    
+    void setIngredientNutrition();
+    const std::string& getLettuceSort() const { return m_lettuceSort; }
+};
+
+}
+
+#endif /* Lettuce_hpp */
