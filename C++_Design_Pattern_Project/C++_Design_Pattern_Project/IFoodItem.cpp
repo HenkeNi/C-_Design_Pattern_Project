2409b74b50f54bbfc5bbdb586825dd795bf08c65
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-30 16:55:47 +0200

    New interface class to be implemented by Burger and Fries classes

---
+++ C++_Design_Pattern_Project/C++_Design_Pattern_Project/IFoodItem.cpp
@@ -0,0 +1,9 @@
+//
+//  IFoodItem.cpp
+//  C++_Design_Pattern_Project
+//
+//  Created by Henrik Jangefelt on 2020-09-30.
+//  Copyright © 2020 Henrik Jangefelt Nilsson. All rights reserved.
+//
+
+#include "IFoodItem.hpp"
---
+++ C++_Design_Pattern_Project/C++_Design_Pattern_Project/IFoodItem.hpp
@@ -0,0 +1,26 @@
+//
+//  IFoodItem.hpp
+//  C++_Design_Pattern_Project
+//
+//  Created by Henrik Jangefelt on 2020-09-30.
+//  Copyright © 2020 Henrik Jangefelt Nilsson. All rights reserved.
+//
+
+#ifndef IFoodItem_hpp
+#define IFoodItem_hpp
+
+#include <stdio.h>
+
+namespace burger_program {
+
+class IFoodItem
+{
+public:
+    virtual ~IFoodItem(){}
+    
+    virtual void eat() const = 0;
+};
+
+}
+
+#endif /* IFoodItem_hpp */
