d906faa0eae8812f7a7f676ff63884993948fcbd
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-30 16:53:28 +0200

    Factory that is in charge of making Beef-Burgers
    Made Hamburger factory abstract

--- C++_Design_Pattern_Project/C++_Design_Pattern_Project/Abstract Factory Pattern/BeefBurgerFactory.cpp
+++ C++_Design_Pattern_Project/C++_Design_Pattern_Project/Abstract Factory Pattern/BeefBurgerFactory.cpp
@@ -20,6 +20,8 @@
 
 #include "NutritionFact.hpp"
 
+#include "BeefBurger.hpp"
+
 #include <vector>
 #include <memory>
 #include <iostream>
@@ -30,13 +32,20 @@ std::unique_ptr<Hamburger> BeefBurgerFactory::makePlainBurger() const
 {
     std::vector<std::unique_ptr<Ingredient>> ingredients{};
     
-    ingredients.push_back(std::make_unique<Bun>("Sesame bun", 90, false));
-    ingredients.push_back(std::make_unique<Patty>("Ground Beef", 120, false));
+    ingredients.push_back(std::make_unique<Bun>("Sesame bun", 90));
+    ingredients.push_back(std::make_unique<Patty>("Ground Beef", 120));
     ingredients.push_back(std::make_unique<Ketchup>("Heintz", 5));
     ingredients.push_back(std::make_unique<Lettuce>("Iceberg", 34));
     ingredients.push_back(std::make_unique<Tomato>("Roma", 14));
     
-    return std::make_unique<Hamburger>("Hamburger", std::move(ingredients));
+    // auto plainBurger = std::make_unique<BeefBurger>("Hamburger", ingredients);
+    //auto plainBurger = std::make_unique<BeefBurger>("Hamburger");
+
+//    std::unique_ptr<BeefBurger> plainBurger = std::make_unique<BeefBurger>("Hamburger");
+//    return plainBurger;
+
+    
+    return std::make_unique<BeefBurger>("Hamburger");
 }
 
 
@@ -44,15 +53,16 @@ std::unique_ptr<Hamburger> BeefBurgerFactory::makeCheeseBurger() const
 {
     std::vector<std::unique_ptr<Ingredient>> ingredients{};
 
-    ingredients.push_back(std::make_unique<Bun>("Potato Bun", 90, false));
-    ingredients.push_back(std::make_unique<Patty>("Ground Beef", 120, false));
-    ingredients.push_back(std::make_unique<Cheese>("Cheddar Cheese", 30, false));
+    ingredients.push_back(std::make_unique<Bun>("Potato Bun", 90));
+    ingredients.push_back(std::make_unique<Patty>("Ground Beef", 120));
+    ingredients.push_back(std::make_unique<Cheese>("Cheddar Cheese", 30));
     ingredients.push_back(std::make_unique<Mustard>("Colman's", 10));
     ingredients.push_back(std::make_unique<Ketchup>("Heinz", 10));
     ingredients.push_back(std::make_unique<Onion>("Yellow Onion", 10));
     ingredients.push_back(std::make_unique<Pickle>(7));
         
-    return std::make_unique<Hamburger>("Cheeseburger",std::move(ingredients));
+    //return std::make_unique<Hamburger>("Cheeseburger",std::move(ingredients));
+    return std::make_unique<BeefBurger>("Cheeseburger");
 }
 
 
@@ -60,15 +70,16 @@ std::unique_ptr<Hamburger> BeefBurgerFactory::makeDoubleExtraBurger() const
 {
     std::vector<std::unique_ptr<Ingredient>> ingredients{};
 
-    ingredients.push_back(std::make_unique<Bun>("Brioche Bun", 90, false));
-    ingredients.push_back(std::make_unique<Patty>("Ground Chuck Steak", 120, false));
-    ingredients.push_back(std::make_unique<Cheese>("Cheddar Cheese", 30, false));
-    ingredients.push_back(std::make_unique<Cheese>("Pepper Jack Cheese", 30, false));
+    ingredients.push_back(std::make_unique<Bun>("Brioche Bun", 90));
+    ingredients.push_back(std::make_unique<Patty>("Ground Chuck Steak", 120));
+    ingredients.push_back(std::make_unique<Cheese>("Cheddar Cheese", 30));
+    ingredients.push_back(std::make_unique<Cheese>("Pepper Jack Cheese", 30));
     ingredients.push_back(std::make_unique<Onion>("White Onion", 10));
     ingredients.push_back(std::make_unique<Pickle>(7));
     ingredients.push_back(std::make_unique<Tomato>("Red Tomatoes", 8));
     
-    return std::make_unique<Hamburger>("Double Deluxe", std::move(ingredients));
+    //return std::make_unique<Hamburger>("Double Deluxe", std::move(ingredients));
+    return std::make_unique<BeefBurger>("Double Deluxe");
 }
 
 }
