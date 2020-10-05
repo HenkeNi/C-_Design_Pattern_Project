56bdb342fee0f2cc07cb701352facc8d9dfd145c
Henrik Jangefelt <henke9105@hotmail.com>
2020-10-05 02:21:40 +0200

    PlainFrenchFries class used in decorator pattern

---
+++ C++_Design_Pattern_Project/C++_Design_Pattern_Project/Decorator Pattern/PlainFrenchFries.cpp
@@ -0,0 +1,22 @@
+//
+//  PlainFrenchFries.cpp
+//  C++_Design_Pattern_Project
+//
+//  Created by Henrik Jangefelt on 2020-09-30.
+//  Copyright © 2020 Henrik Jangefelt Nilsson. All rights reserved.
+//
+
+#include "PlainFrenchFries.hpp"
+#include <iostream>
+
+namespace burger_program {
+
+void PlainFrenchFries::make() const
+{
+    std::cout << "Making Fries\n";
+}
+
+
+
+
+}
---
+++ C++_Design_Pattern_Project/C++_Design_Pattern_Project/Decorator Pattern/PlainFrenchFries.hpp
@@ -0,0 +1,26 @@
+//
+//  PlainFrenchFries.hpp
+//  C++_Design_Pattern_Project
+//
+//  Created by Henrik Jangefelt on 2020-09-30.
+//  Copyright © 2020 Henrik Jangefelt Nilsson. All rights reserved.
+//
+
+#ifndef PlainFrenchFries_hpp
+#define PlainFrenchFries_hpp
+
+#include "IFrenchFries.hpp"
+#include <stdio.h>
+
+namespace burger_program {
+
+class PlainFrenchFries : public IFrenchFries
+{
+    
+public:
+    virtual void make() const override;
+};
+
+}
+
+#endif /* PlainFrenchFries_hpp */
